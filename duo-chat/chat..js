e4f9e55d10176144e5efc8d686f325c0aca3e18a
h-k-dev <hongknop@yahoo.com>
2023-03-28 20:44:15 +0200

    Seperate
    Seperate comparing unit from computation unit.

--- duo-chat/chat..js
+++ duo-chat/chat..js
@@ -1,195 +1,212 @@
-class ChatCell {
-/** 
- * @param {obj} - All below param are incl. in the object as `property`
- * @param {string} containerId  - TO DO
- * @param {string} paddingCss - TO DO.
- * @param {string} scrollCss - TO DO.
- * @param {string} msgCss - TO DO.
- * @param {string} typographyCss - TO DO.
- * @param {string} computationCss - TO DO.
- * ------
- * @param {string} useBionic - TO DO
- * @param {string} useAutoScroll - TO DO
- * */ 
-	constructor(
-		obj,
-		useBionic = False,
-		useAutoScroll = True
-	) {	
-		// Parent container
-		this.container = document.getElementById(obj["containerId"]);
+class Chat {
+	/** 
+	 * @param {obj} - All below param are incl. in the object as `property`
+	 * @param {string} containerId  - TO DO
+	 * @param {string} paddingCss - TO DO.
+	 * @param {string} scrollCss - TO DO.
+	 * @param {string} msgCss - TO DO.
+	 * @param {string} typographyCss - TO DO.
+	 * @param {string} compareCss - TO DO.
+	 * */ 
+		constructor(obj) {	
+			// Parent container
+			this.container = document.getElementById(obj["containerId"]);
+	
+			// Display relevant elements.
+			this.padding = document.createElement("div");
+			this.scroll = document.createElement("div");
+			this.msg = document.createElement("div");
+	
+			// Computation relevant elements.
+			this.computor = document.createElement("div");
+			this.comparor = document.createElement("div");
+			
+			this.lineHeight = null;
+			// Comparing attributes
+			this.maxNumLines = null;
+			this.maxLinesHeight = null;
+			// This Cell attribute
+			this._maxNumLines = null;
+			this._maxLinesHeight = null;
 
-		// Display relevant elements.
-		this.padding = document.createElement("div");
-		this.scroll = document.createElement("div");
-		this.msg = document.createElement("div");
+	
+			// Auto Invoke
+			this.adjustContainer(obj);
+	
+			// Messagge queue
+			this.queue = [];
+			
+			// States
+			this.curMsg = null;
+			this.idleTimer = this.retriggerAnimation(this.padding, "blend-out", "blend-in");
+		};
+	
+	
+		adjustContainer(obj)
+		{	
+			// Display relevant elements.
+			this.padding.classList.add(obj["paddingCss"]);
+			this.scroll.classList.add(obj["scrollCss"]);
+			this.msg.classList.add(obj["msgCss"]);
+			this.msg.classList.add(obj["typographyCss"]);
+	
+			// Computation relevant elements.
+			this.computor.classList.add(obj["computeCss"], obj["typographyCss"]);
+			this.comparor.classList.add(obj["compareCss"], obj["typographyCss"])
 
-		// Computation relevant elements.
-		this.computor = document.createElement("div");
-
-		// Computed Attr
-		this.lineHeight = null;
-		this.maxNumlines = null;
-		this.maxLinesHeight = null;
-
-		// Auto Invoke
-		this.adjustContainer(obj);
-
-		// Messagge queue
-		this.queue = [];
-		
-		// States
-		this.curMsg = null;
-		this.idleTimer = this.retriggerAnimation(this.padding, "blend-out", "blend-in");
-
-		// Options
-		this.useBionic = useBionic;
-		this.useAutoScroll = useAutoScroll;
-	};
-
-
-	adjustContainer(obj)
-	{	
-		// Display relevant elements.
-		this.padding.classList.add(obj["paddingCss"]);
-		this.scroll.classList.add(obj["scrollCss"]);
-		this.msg.classList.add(obj["msgCss"]);
-		this.msg.classList.add(obj["typographyCss"]);
-
-		// Computation relevant elements.
-		this.computor.classList.add(obj["computationCss"], obj["typographyCss"]);
-
-		// Nesting
-		this.scroll.appendChild(this.msg);
-		this.padding.appendChild(this.scroll);
-		this.container.appendChild(this.padding);
-
-		document.body.appendChild(this.computor);
-	};
-
-
-	computeLineHeight() {
-		const fontSize = parseFloat(this.getcAtr(this.computor, "font-size"));
-		
+			// Nesting
+			this.scroll.appendChild(this.msg);
+			this.padding.appendChild(this.scroll);
+			this.container.appendChild(this.padding);
+	
+			document.body.appendChild(this.computor);
+			document.body.appendChild(this.comparor);
+		};
+	
+	
+		computeLineHeight() {
+			const fontSize = parseFloat(this.getcAtr(this.computor, "font-size"));
+			
 			// Get and adjust
-		this.lineHeight = this.getcAtr(this.computor, "line-height");
-		this.lineHeight = Math.ceil(parseFloat(this.lineHeight));
+			this.lineHeight = this.getcAtr(this.computor, "line-height");
+			this.lineHeight = Math.ceil(parseFloat(this.lineHeight));
+				
+			// Update style
+			document.querySelector(':root').style.setProperty(
+			"--line-height", this.lineHeight / fontSize);
+		};
+	
+	
+		compareMaxHeight(maxNumChar = 500) {
+			const testStr = "W".repeat(maxNumChar);
+			const paddingTop = parseInt(this.getcAtr(this.padding, "padding-top"));
+			const paddingBot = parseInt(this.getcAtr(this.padding, "padding-bottom"));
+			let height;
 			
-		// Update style
-		document.querySelector(':root').style.setProperty(
-		"--line-height", this.lineHeight / fontSize);
-	};
+			// Adding testing content
+			this.comparor.innerHTML = testStr;
+	
+			height = this.comparor.clientHeight - paddingTop - paddingBot;
+			this.maxNumLines = Math.floor(height / this.lineHeight);
+			this.maxLinesHeight = this.maxNumLines * this.lineHeight;
+				
+			this.comparor.style.maxheight = String(this.maxLinesHeight) + "px";
+			this.padding.style.maxHeight = String(this.maxLinesHeight) + "px";
+		};
 
 
-	computeMaxHeight(maxNumChar = 500) {
-		const testStr = "W".repeat(maxNumChar);
-		const paddingTop = parseInt(this.getcAtr(this.padding, "padding-top"));
-		const paddingBot = parseInt(this.getcAtr(this.padding, "padding-bottom"));
-		let height;
-		
-		// Adding testing content
-		this.computor.innerHTML = testStr;
-
-		height = this.computor.clientHeight - paddingTop - paddingBot;
-		this.maxNumlines = Math.floor(height / this.lineHeight);
-		this.maxLinesHeight = this.maxNumlines * this.lineHeight;
+		computeMaxHeight(maxNumChar = 500){
+			const testStr = "W".repeat(maxNumChar);
+			const paddingTop = parseInt(this.getcAtr(this.padding, "padding-top"));
+			const paddingBot = parseInt(this.getcAtr(this.padding, "padding-bottom"));
+			let height;
 			
-		this.computor.style.maxheight = String(this.maxLinesHeight) + "px";
-		this.padding.style.maxHeight = String(this.maxLinesHeight) + "px";
-
-		this.setCssVar({
-			"--scroll-len": String(this.maxLinesHeight) + "px",
-			"--scroll-duration": String(this.maxLinesHeight / 60) + "s"});
-	};
-
-
-	analyze(msg) {
-		let scrollHeight;			
-
-		this.computor.innerHTML = msg.asHTML;
-		scrollHeight = this.computor.scrollHeight;
+			// Adding testing content
+			this.computor.innerHTML = testStr;
+	
+			height = this.computor.clientHeight - paddingTop - paddingBot;
+			this._maxNumLines = Math.floor(height / this.lineHeight);
+			this._maxLinesHeight = this.maxNumLines * this.lineHeight;
+				
+			this.computor.style.maxheight = String(this.maxLinesHeight) + "px";
+			this.padding.style.maxHeight = String(this.maxLinesHeight) + "px";
+	
+			this.setCssVar({
+				"--scroll-len": String(this.maxLinesHeight) + "px",
+				"--scroll-duration": String(this.maxLinesHeight / 60) + "s"});
+		};
+	
+	
+		analyze(msg) {
+			let scrollHeight;			
+	
+			this.computor.innerHTML = msg.asHTML;
+			scrollHeight = this.computor.scrollHeight;
+			
+			msg.numLines = Math.floor(scrollHeight / this.lineHeight);
+			msg.numScroll = Math.ceil(scrollHeight / this.maxLinesHeight) - 1;
+			msg.scrollStart = this.maxLinesHeight;
+		};
+	
+	
+		autoScroll() {
+			
+			if (this.curMsg.numScroll === 0) return this.dequeue();
+	
+			this.padding.style.height = String(this.maxLinesHeight) + "px";
+			this.curMsg.numScroll -= 1;
+			this.curMsg.scrollStart -= this.maxLinesHeight;
+			
+			this.setCssVar({"--scroll-start": String(this.curMsg.scrollStart) + "px"})
+	
+			// No animation time involve
+			this.retriggerAnimation(this.msg, "scroll-down", "scroll-down")
+			this.idleTimer = window.setTimeout(this.autoScroll.bind(this), 5000);
+		};
 		
-		msg.numLines = Math.floor(scrollHeight / this.lineHeight);
-		msg.numScroll = Math.ceil(scrollHeight / this.maxLinesHeight) - 1;
-		msg.scrollStart = this.maxLinesHeight;
-	};
-
-
-	autoScroll() {
-		
-		if (this.curMsg.numScroll === 0) return this.dequeue();
-
-		this.padding.style.height = String(this.maxLinesHeight) + "px";
-		this.curMsg.numScroll -= 1;
-		this.curMsg.scrollStart -= this.maxLinesHeight;
-		
-		this.setCssVar({"--scroll-start": String(this.curMsg.scrollStart) + "px"})
-
-		// No animation time involve
-		this.retriggerAnimation(this.msg, "scroll-down", "scroll-down")
-		this.idleTimer = window.setTimeout(this.autoScroll.bind(this), 5000);
+	
+		enqueue(msg) {
+			this.analyze(msg)
+			this.queue.push(msg);
+			if (this.curMsg === null) {
+				clearTimeout(this.idleTimer);
+				this.peak()
+			};
+		};
+	
+	
+		peak() {
+			this.curMsg = this.queue[0];
+			this.msg.classList.remove("scroll-down");
+			if (this.curMsg.numScroll > 0) {
+				this.padding.style.height = `${this.maxLinesHeight}px`;
+				this.msg.innerHTML = this.curMsg.asHTML;
+				this.retriggerAnimation(this.padding, "blend-in", "blend-out");
+				this.idleTimer = window.setTimeout(this.autoScroll.bind(this), 5000)
+			} else {
+				this.padding.style.height = "auto";
+				this.msg.innerHTML = this.curMsg.asHTML;
+				this.retriggerAnimation(this.padding, "blend-in", "blend-out");
+				this.dequeue();
+			};	
+		};
+	
+	
+		dequeue() {
+			console.log("in dequeue");
+			this.queue.shift();
+			
+			if (this.queue.length > 0) {
+				this.peak() }
+			else {
+				this.curMsg = null;
+				this.idleTimer = window.setTimeout(this.idle.bind(this), 5000);
+			};
+		};
+	
+	
+		retriggerAnimation(element, in_, out_) {
+			console.log("Ani", element);
+			element.classList.remove(out_);
+			void element.offsetHeight;
+			element.classList.add(in_);
+		};
+	
+	
+		idle() {
+			this.retriggerAnimation(this.padding, "blend-out", "blend-in");
+		};
+	
+	
+		getcAtr(element, attribute) {
+			return window.getComputedStyle(element).getPropertyValue(attribute)
+		};
+	
+	
+		setCssVar(obj) {
+			for (const [varName, value] of Object.entries(obj)) {
+				document.documentElement.style.setProperty(varName, value);
+			};
+		};
 	};
 	
-
-	enqueue(msg) {
-		this.analyze(msg)
-		this.queue.push(msg);
-		if (this.curMsg === null) {
-			clearTimeout(this.idleTimer);
-			this.peak()
-		};
-	};
-
-
-	peak() {
-		this.curMsg = this.queue[0];
-		this.msg.classList.remove("scroll-down");
-		if (this.curMsg.numScroll > 0 && this.useAutoScroll) {
-			this.padding.style.height = `${this.maxLinesHeight}px`;
-			this.msg.innerHTML = this.curMsg.asHTML;
-			this.retriggerAnimation(this.padding, "blend-in", "blend-out");
-			this.idleTimer = window.setTimeout(this.autoScroll.bind(this), 5000)
-		} else {
-			this.padding.style.height = "auto";
-			this.msg.innerHTML = this.curMsg.asHTML;
-			this.retriggerAnimation(this.padding, "blend-in", "blend-out");
-			this.dequeue()
-		};	
-	};
-
-
-	dequeue() {
-		this.queue.shift();
-		
-		if (this.queue.length > 0) {
-			this.peak() }
-		else {
-			this.curMsg = null;
-			this.idleTimer = window.setTimeout(this.idle.bind(this), 5000);
-		};
-	};
-
-
-	retriggerAnimation(element, in_, out_) {
-		element.classList.remove(out_);
-		void element.offsetHeight;
-		element.classList.add(in_);
-	};
-
-
-	idle() {
-		this.retriggerAnimation(this.padding, "blend-out", "blend-in");
-	};
-
-
-	getcAtr(element, attribute) {
-		return window.getComputedStyle(element).getPropertyValue(attribute)
-	};
-
-
-	setCssVar(obj) {
-		for (const [varName, value] of Object.entries(obj)) {
-			document.documentElement.style.setProperty(varName, value);
-		};
-	};
-};
